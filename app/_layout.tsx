dace15ae6e2a9a7ef5e19e47e8565c1f97093f06
Anthony Sawyer <sawyer.developer@gmail.com>
2025-03-04 15:01:37 -0500

    refactor for better commands

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font"
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/Roboto-Medium.ttf'),
+    Roboto: require('../assets/fonts/Roboto-Medium.ttf'),
   });
   return <Stack />;
 }
