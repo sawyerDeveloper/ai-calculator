98f441eee9f0e0715645f28629c0be076a945ae6
Anthony Sawyer <sawyer.developer@gmail.com>
2025-03-08 07:05:44 -0500

    better header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { Stack } from "expo-router";
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen options={{title:"AI Calculator"}} name="index"></Stack.Screen>
+  </Stack>;
 }
