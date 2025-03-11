f5301c217ebfab0fc9bdd280d75bd90fd9d47c76
Anthony Sawyer <sawyer.developer@gmail.com>
2025-03-10 21:35:27 -0400

    header style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen options={{title:"AI Calculator"}} name="index"></Stack.Screen>
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen
+        options={{
+          title: 'AI Calculator',
+          headerTitleAlign: 'center',
+          headerTitleStyle: { fontSize: 30 },
+        }}
+        name='index'
+      ></Stack.Screen>
+    </Stack>
+  );
 }
