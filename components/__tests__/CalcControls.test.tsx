19340ef80be21bde462899410939e1c538940f11
Anthony Sawyer <sawyer.developer@gmail.com>
2025-03-14 11:07:53 -0400

    refactoring file structure

--- components/__tests__/CalcControls.test.tsx
+++ components/__tests__/CalcControls.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react-native';
-import CalcControls from './CalcControls';
+import CalcControls from '../CalcControls';
 
 it('renders correctly', () => {
   const tree = render(
